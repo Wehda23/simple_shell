f82dcc48c099fa6bf68861e8c3696d953d912726
Waheed Khaled <waheedkhaled95@gmail.com>
2023-11-14 18:21:14 +0200

    hotfix

--- env_commands.c
+++ env_commands.c
@@ -6,13 +6,15 @@ void print_env(char *file)
 {
     pid_t pid = fork();
     int status;
+    char **env;
+    
     if (pid == -1)
     {
         perror(file);
     } 
     else if (pid == 0)
     {
-        for (char **env = environ; *env != NULL; env++)
+        for (env = environ; *env != NULL; env++)
         {
             printf("%s\n", *env); /* Print each environment variable */
         }
