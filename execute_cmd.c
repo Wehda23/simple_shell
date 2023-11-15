9afd9724d78ffcfdc961674928b3be9b6f44baa5
Waheed Khaled <waheedkhaled95@gmail.com>
2023-11-15 20:25:00 +0200

    added execute command

--- execute_cmd.c
+++ execute_cmd.c
@@ -7,7 +7,7 @@
  */
 void execute_command(char **cmd, char *file)
 {
-    pid_t pid, wpid;
+    pid_t pid;
     int status;
 
     pid = fork();
@@ -17,14 +17,16 @@ void execute_command(char **cmd, char *file)
             perror(file);
         }
         exit(EXIT_FAILURE);
-    } else if (pid < 0)
+    } 
+    else if (pid < 0)
     {
         perror(file);
-    } else {
+    } 
+    else
+    {
 
-        do
-        {
-            wpid = waitpid(pid, &status, WUNTRACED);
+        do {
+            waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 }
